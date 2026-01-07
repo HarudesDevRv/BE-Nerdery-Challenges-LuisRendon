03681722ccb2497953b6e24532f1bde3f1cd6f6b
HarudesDevRv <luisrendon@ravn.co>
2026-01-07 16:08:00 -0500

    Asynchronous programming challenge 1 initial solution

--- 1-js-and-async-programming/2-asynchronous-js/1-callback.js
+++ 1-js-and-async-programming/2-asynchronous-js/1-callback.js
@@ -22,6 +22,27 @@ const makeRequest = require("./utils/make-requests");
 
 const makeRequestWithRetries = (attempts) => {
   // TODO: Implement the function to make a request and retry if it fails
+  function myCallback(err,data){
+    if(err){
+      let message = err?.message;
+      let tries = parseInt(message.slice(26));
+      if(tries<attempts){
+          return makeRequest(tries+1,myCallback);
+      }
+      else{
+        console.log("All attempts failed.");
+        return Error("All attempts failed.");
+      }
+    }else if(data){
+      console.log(data);
+      return data;
+    }else{
+      console.log("Something went wrong");
+      return Error("Something went wrong");
+    }
+  }
+
+  makeRequest(1,myCallback);
 };
 
 makeRequestWithRetries(10);
