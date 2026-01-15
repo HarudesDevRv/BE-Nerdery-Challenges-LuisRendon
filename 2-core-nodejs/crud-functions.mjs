bb1265edd7e0e9eb093bbcf66b6da7269e54a781
HarudesDevRv <luisrendon@ravn.co>
2026-01-15 12:57:07 -0500

    Refactor: Improved data validation, improved messages on bad data input and changed functions to support those changes

--- 2-core-nodejs/crud-functions.mjs
+++ 2-core-nodejs/crud-functions.mjs
@@ -1,6 +1,6 @@
-import fs from 'fs/promises';
+import fs from "fs/promises";
 
-const wishlistPath = 'wishlist.json';
+const wishlistPath = "wishlist.json";
 
 /**
  * @typedef {Object} Item
@@ -11,165 +11,226 @@ const wishlistPath = 'wishlist.json';
  */
 
 /**
- * Validates the item object structure
- * @param {Item} item 
- * The item to be validated
+ * @typedef {Object} Summary
+ * @property {number} itemCount - The amount of items on the wishlist.
+ * @property {number} total - The sum of the wishlist items values.
+ * @property {Item} mostExpensiveItem - The wishlist most expensive item.
  */
-function validateItem(item){
-    return Object.hasOwn(item,"name") && typeof(item.name) == "string" &&
-     Object.hasOwn(item,"price") && typeof(item.price) == "number" && 
-     Object.hasOwn(item,"store") && typeof(item.store) == "string";
+
+/**
+ * Validates the item object structure
+ * @param {Item} item
+ * The item to be validated
+ * @returns
+ * Return a boolean with the validation result
+ */
+function validateItem(item) {
+  return (
+    Object.hasOwn(item, "name") &&
+    typeof item.name == "string" &&
+    Object.hasOwn(item, "price") &&
+    typeof item.price == "number" &&
+    Object.hasOwn(item, "store") &&
+    typeof item.store == "string"
+  );
 }
 
 /**
- * Creates a new item on the wishlist
- * @param {Item} item 
- * The item to be created on the wishlist
+ * Validates the summary object structure
+ * @param {Summary} item
+ * The summary to be validated
+ * @returns
+ * Return a boolean with the validation result
  */
-export async function createWishlistItem(item){
-    if(!validateItem(item)){//Show an alert when the item data is not valid
-        console.log("Please enter a valid item");
-        return;
-    }
-    await fs.readFile(wishlistPath, { encoding: 'utf8' }).then(async data=>{
-        let wishlist = JSON.parse(data);//Get and update the wishlist
-        wishlist.items.push({id:wishlist.autoincrement,...item,});
-        wishlist.autoincrement++;
-        await fs.writeFile(wishlistPath,JSON.stringify(wishlist)).then(()=>{//Save the updated wishlist
-            console.log("Wishlist updated");
-        }).catch(err=>{
-            console.log(err);
-        });
-    }).catch(err=>{
-        console.log(err);
-    });
+function validateSummary(summary) {
+  return (
+    Object.hasOwn(summary, "itemCount") &&
+    typeof summary.itemCount == "number" &&
+    Object.hasOwn(summary, "total") &&
+    typeof summary.total == "number" &&
+    Object.hasOwn(summary, "mostExpensiveItem") &&
+    validateItem(summary.mostExpensiveItem)
+  );
+}
+
+/**
+ * Process the wishlist summary data
+ * @param {Summary} summary
+ * An object that contains the most expensive item, the total cost and the item cost of the wishlist.
+ * it must have the mostExpensiveItem, itemCount and total properties
+ * @returns
+ * Return a string that contains the formatted summary data
+ */
+const createSummary = (summary) =>
+  validateSummary(summary)
+    ? `This is your summary:
+Most expensive item:\t${summary.mostExpensiveItem.name},` +
+      ` ${summary.mostExpensiveItem.price.toFixed(2)}` +
+      ` at ${summary.mostExpensiveItem.store}
+Total cost:\t\t${summary.total.toFixed(2)}
+Number of items:\t${summary.itemCount}
+Average price:\t\t${(summary.total / summary.itemCount).toFixed(2)}`
+    : "Invalid summary data";
+
+/**
+ * Creates a new item on the wishlist
+ * @param {Item} item
+ * The item to be created on the wishlist
+ * @returns
+ * Returns a success message or an error message
+ */
+export async function createWishlistItem(item) {
+  if (!validateItem(item)) return "Please enter a valid item";
+
+  let data = await fs.readFile(wishlistPath, { encoding: "utf8" });
+  let wishlist = JSON.parse(data); //Get and update the wishlist
+  wishlist.items.push({ id: wishlist.autoincrement, ...item });
+  wishlist.autoincrement++;
+  let result = await fs
+    .writeFile(wishlistPath, JSON.stringify(wishlist))
+    .then(() => "Wishlist updated")
+    .catch((err) => err.message);
+  return result;
 }
 
 /**
  * Reads the wishlist and prints it on the console
+ * @returns
+ * Returns a success message or an error message
  */
-export async function readWishlist(){
-    await fs.readFile(wishlistPath, { encoding: 'utf8' }).then(data=>{
-        let wishlist = JSON.parse(data);//Get and show the wishlist
-        if(wishlist.items.length){
-            for(let item of wishlist.items){
-                console.log(`${item.id}\tName: ${item.name}\tPrice: ${item.price.toFixed(2)}\tStore: ${item.store}`);
-            }
-        }else{
-            console.log("You have no items on your wishlist");
-        }
-    }).catch(err=>{
-        console.log(err);
-    });
+export async function readWishlist() {
+  let data = await fs.readFile(wishlistPath, { encoding: "utf8" });
+  let wishlist = JSON.parse(data); //Get and show the wishlist
+  let result = "You don't have any item on your wishlist";
+  if (wishlist.items.length) {
+    for (let item of wishlist.items) {
+      console.log(
+        `${item.id}\tName: ${item.name}\tPrice: ${item.price.toFixed(2)}\tStore: ${item.store}`,
+      );
+    }
+    result = "Items printed successfully";
+  }
+  return result;
 }
 
 /**
  * Removes an item from the wishlist if found
- * @param {number} id 
+ * @param {number} id
  * The ID of the item to be removed
+ * @returns
+ * Returns a success message or an error message
  */
-export async function removeWishlistItem(id){
-    await fs.readFile(wishlistPath,{encoding:'utf8'}).then(async data=>{
-        let wishlist = JSON.parse(data);//Get the wishlist and search for the item by ID
-        let items = wishlist.items;
-        let itemIndex = items.findIndex(item=>item.id==id);
-        if(itemIndex>=0){//If found, remove the item from the wishlist
-            items.splice(itemIndex,1);
-            await fs.writeFile(wishlistPath,JSON.stringify(wishlist)).then(()=>{//Save the updatet wishlist
-                console.log("wishlist updated");
-            }).catch(err=>{
-                console.log(err);
-            });
-        }
-        else{
-            console.log("Item not found");
-        }
-    }).catch(err=>{
-        console.log(err);
-    });
+export async function removeWishlistItem(id) {
+  let data = await fs.readFile(wishlistPath, { encoding: "utf8" });
+  let wishlist = JSON.parse(data); //Get the wishlist and search for the item by ID
+  let items = wishlist.items;
+  let itemIndex = items.findIndex((item) => item.id == id);
+  let result = "Item not found";
+  if (itemIndex >= 0) {
+    //If found, remove the item from the wishlist
+    items.splice(itemIndex, 1);
+    result = await fs
+      .writeFile(wishlistPath, JSON.stringify(wishlist))
+      .then(() => "wishlist updated")
+      .catch((err) => err.message);
+  }
+  return result;
 }
 
 /**
  * Updates an item from the wishlist if found with the provided data
- * @param {number} id 
+ * @param {number} id
  * The ID of the item to be updated
- * @param {Item} updatedItem 
+ * @param {Item} updatedItem
  * The new item data
- * @returns 
+ * @returns
+ * Returns a success message or an error message
  */
-export async function updateWishlistItem(id, updatedItem){
-    if(!validateItem(updatedItem)){//Show an alert when the item data is not valid
-        console.log("Please enter a valid item");
-        return;
-    }
-    await fs.readFile(wishlistPath,{encoding:'utf8'}).then(async data=>{
-        let wishlist = JSON.parse(data);//Get the wishlist and search for the item by ID
-        let items = wishlist.items;
-        let itemIndex = items.findIndex(item=>item.id==id);
-        if(itemIndex>=0){//If found, update the item
-            items[itemIndex]={id:id,...updatedItem};
-            await fs.writeFile(wishlistPath,JSON.stringify(wishlist)).then(()=>{//Save the updated wishlist
-                console.log("Wishlist updated");
-            }).catch(err=>{
-                console.log(err);
-            });
-        }
-        else{
-            console.log("Item not found");
-        }
-    }).catch(err=>{
-        console.log(err);
-    });
+export async function updateWishlistItem(id, updatedItem) {
+  if (!validateItem(updatedItem)) return "Please enter a valid item";
+
+  let data = await fs.readFile(wishlistPath, { encoding: "utf8" });
+  let wishlist = JSON.parse(data); //Get the wishlist and search for the item by ID
+  let items = wishlist.items;
+  let itemIndex = items.findIndex((item) => item.id == id);
+  let result = "Item not found";
+  if (itemIndex >= 0) {
+    //If found, update the item
+    items[itemIndex] = { id: id, ...updatedItem };
+    result = await fs
+      .writeFile(wishlistPath, JSON.stringify(wishlist))
+      .then(() => "Wishlist updated")
+      .catch((err) => err.message);
+  }
+  return result;
 }
 
 /**
  * Shows the summary of the wishlist: Most expensive item, total cost, number of items and average cost
+ * @returns
+ * Returns the formatted summary or an error message
  */
-export async function showWishlistSummary(){
-    await fs.readFile(wishlistPath, { encoding: 'utf8' }).then(data=>{
-        let wishlist = JSON.parse(data);//Get the wishlist and set variables to keep necessary data
-        let total = 0;
-        let mostExpensiveItem = {};
-        let mostExpensivePrice= 0;
-        let itemCount = wishlist.items.length;
-        if(itemCount){//If there is at least one item, calculate the total price and most expensive item
-            for(let item of wishlist.items){
-                total += item.price;
-                if(item.price > mostExpensivePrice){
-                    mostExpensivePrice = item.price;
-                    mostExpensiveItem = {...item};
-                }
-            }
-            console.log(//Calculate an show the summary
-`This is your summary:
-Most expensive item:\t${mostExpensiveItem.name}, ${mostExpensiveItem.price.toFixed(2)} at ${mostExpensiveItem.store}
-Total cost:\t\t${total.toFixed(2)}
-Number of items:\t${itemCount}
-Average price:\t\t${(total/itemCount).toFixed(2)}`);
-        }else{
-            console.log("You have no items on your wishlist");
-        }
-    }).catch(err=>{
-        console.log(err);
+export async function showWishlistSummary() {
+  let data = await fs.readFile(wishlistPath, { encoding: "utf8" });
+  let wishlist = JSON.parse(data); //Get the wishlist and set variables to keep necessary data
+  let total = 0;
+  let mostExpensiveItem = {};
+  let mostExpensivePrice = 0;
+  let itemCount = wishlist.items.length;
+  let summary = "You don't have any item on your wishlist";
+  if (itemCount) {
+    //If there is at least one item, calculate the total price and most expensive item
+    for (let item of wishlist.items) {
+      total += item.price;
+      if (item.price > mostExpensivePrice) {
+        mostExpensivePrice = item.price;
+        mostExpensiveItem = { ...item };
+      }
+    }
+    summary = createSummary({
+      mostExpensiveItem,
+      total,
+      itemCount,
     });
+  }
+  return summary;
 }
 
 /**
  * Exports the wishlist as a CSV file and saves it locally
+ * @returns
+ * Returns a success message or an error message
  */
-export async function exportAsCSV(){
-    await fs.readFile(wishlistPath, { encoding: 'utf8' }).then(async data=>{
-        let wishlist = JSON.parse(data);//Get the wishlist and set the CSV header
-        let csv = "Item,Name,Price,Store";
-        for(let item of wishlist.items){//Add each item data to the csv
-            csv+='\n' + item.id.toString() + "," + item.name + "," + item.price.toString() + ","+item.store;
-        }
-        await fs.writeFile('wishlist.csv',csv).then(()=>{//Locally save the CSV on the code folder
-            console.log("Wishlist successfully exported as CSV");
-        }).catch(err=>{
-            console.log(err);
-        });
-    }).catch(err=>{
-        console.log(err);
-    });
+export async function exportAsCSV() {
+  let data = await fs.readFile(wishlistPath, { encoding: "utf8" });
+  let wishlist = JSON.parse(data); //Get the wishlist and set the CSV header
+  let csv = "Item,Name,Price,Store";
+  for (let item of wishlist.items) {
+    //Add each item data to the csv
+    csv +=
+      "\n" +
+      item.id.toString() +
+      "," +
+      item.name +
+      "," +
+      item.price.toString() +
+      "," +
+      item.store;
+  }
+  let result = await fs
+    .writeFile("wishlist.csv", csv)
+    .then(() => "Wishlist successfully exported as CSV")
+    .catch((err) => err.message);
+  return result;
+}
+
+export async function getItemById(id) {
+  let data = await fs.readFile(wishlistPath, { encoding: "utf8" });
+  let wishlist = JSON.parse(data); //Get the wishlist and search for the item by ID
+  let items = wishlist.items;
+  let itemIndex = items.findIndex((item) => item.id == id);
+  if (itemIndex >= 0) {
+    return items[itemIndex];
+  }
+  return null;
 }
--- 2-core-nodejs/wishlist-tracker.js
+++ 2-core-nodejs/wishlist-tracker.js
@@ -1,14 +1,17 @@
-import {readWishlist,
-    createWishlistItem,
-    removeWishlistItem,
-    updateWishlistItem,
-    showWishlistSummary,
-    exportAsCSV} from './crud-functions.mjs';
-import fs from 'fs/promises';
-import readline from 'readline/promises';
+import {
+  readWishlist,
+  createWishlistItem,
+  removeWishlistItem,
+  updateWishlistItem,
+  showWishlistSummary,
+  exportAsCSV,
+  getItemById,
+} from "./crud-functions.mjs";
+import fs from "fs/promises";
+import readline from "readline/promises";
 
-//Message to show when no aditional value is passed
-const welcomeMessage =`Welcome to the Wishlist Tracker
+//Message to show when no additional value is passed
+const welcomeMessage = `Welcome to the Wishlist Tracker
 Use the following flags if you want to to interact with your wishlist on the command line:
 See the items of your wishlist:\t -r
 Add a new item to your wishlist: -c --name <name> --price <price> --store <store>
@@ -24,187 +27,223 @@ const args = process.argv;
  * @param {string[]} validationArray
  * An array with the name of the arguments to be validated
  */
-function validateArgs(validationArray){
-    for(let validation of validationArray){
-        let argumentIndex = args.indexOf(validation)
-        //validate the argument name is specified and the next argument exists and is not another argument name
-        if(argumentIndex <0 || argumentIndex == args.length-1 || args[argumentIndex+1].indexOf("-")>-1){
-            return false;
-        }
+function validateArgs(validationArray) {
+  for (let validation of validationArray) {
+    let argumentIndex = args.indexOf(validation);
+    //validate the argument name is specified and the next argument exists and is not another argument name
+    if (
+      argumentIndex < 0 ||
+      argumentIndex == args.length - 1 ||
+      args[argumentIndex + 1].indexOf("-") > -1
+    ) {
+      return false;
     }
-    return true;
+  }
+  return true;
 }
 
-const getArgsValue = value => args[args.indexOf(value)+1];
+const getArgsValue = (value) => args[args.indexOf(value) + 1];
 
 /**
  * Interprets what command to execute on a call with arguments
  */
-function commandLineInterpreter(){
-    switch(args[2]){
-        case "-c": //Create a wishlist item
-            if(validateArgs(["--name","--store","--price"])){
-                createWishlistItem({
-                    name:getArgsValue("--name"),
-                    price:parseFloat(getArgsValue("--price")),
-                    store:getArgsValue("--store"),
-                });
-            }else{
-                console.log("Please enter a valid item");
-            }
-            break;
-        case "-r": //Read the wishlist items
-            readWishlist();
-            break;
-        case "-u": //Update a wishlist item
-            if(validateArgs(["--id","--name","--store","--price"])){
-                updateWishlistItem(parseInt(getArgsValue("--id")),{
-                    name:getArgsValue("--name"),
-                    price:parseFloat(getArgsValue("--price")),
-                    store:getArgsValue("--store"),
-                });
-            }else{
-                console.log("Please enter a valid item and id");
-            }
-            break;
-        case "-d": //Delete a wishlist item
-            if(validateArgs(["--id"])){
-                removeWishlistItem(getArgsValue("--id"));
-            }else{
-                console.log("Please enter a valid item id");
-            }
-            break;
-        case "-s": //Show the wishlist summary
-            showWishlistSummary();
-            break;
-        case "-e": //Export the wishlist as CSV
-            exportAsCSV();
-            break;
-        default:
-            console.log("Please enter a valid command");
-            break;
-    }
-    //
+async function commandLineInterpreter() {
+  let consoleMessage = "";
+  switch (args[2]) {
+    case "-c": //Create a wishlist item
+      if (validateArgs(["--name", "--store", "--price"])) {
+        consoleMessage = await createWishlistItem({
+          name: getArgsValue("--name"),
+          price: parseFloat(getArgsValue("--price")),
+          store: getArgsValue("--store"),
+        });
+      } else {
+        consoleMessage = "Please enter a valid item";
+      }
+      break;
+    case "-r": //Read the wishlist items
+      consoleMessage = await readWishlist();
+      break;
+    case "-u": //Update a wishlist item
+      if (validateArgs(["--id", "--name", "--store", "--price"])) {
+        consoleMessage = await updateWishlistItem(
+          parseInt(getArgsValue("--id")),
+          {
+            name: getArgsValue("--name"),
+            price: parseFloat(getArgsValue("--price")),
+            store: getArgsValue("--store"),
+          },
+        );
+      } else {
+        consoleMessage = "Please enter a valid item and id";
+      }
+      break;
+    case "-d": //Delete a wishlist item
+      if (validateArgs(["--id"])) {
+        consoleMessage = await removeWishlistItem(getArgsValue("--id"));
+      } else {
+        consoleMessage = "Please enter a valid item id";
+      }
+      break;
+    case "-s": //Show the wishlist summary
+      consoleMessage = await showWishlistSummary();
+      break;
+    case "-e": //Export the wishlist as CSV
+      consoleMessage = await exportAsCSV();
+      break;
+    default:
+      consoleMessage = "Please enter a valid command";
+      break;
+  }
+  console.log(consoleMessage);
+  //
+}
+
+/**
+ * Creates an item by asking questions on the console, can provide an item to compare with the new one
+ * @param {readline.Interface} readLine
+ * The Readline Interface that will receive the data and show the messages
+ * @returns {Promise<Item>}
+ * Returns an Item with the entered data
+ */
+async function createItemOnConsole(readLine, oldItem) {
+  let name = (
+    await readLine.question(
+      "Enter the item name:\n" + (oldItem ? `(${oldItem.name}) ` : ""),
+    )
+  ).trim();
+  while (name === "") {
+    name = (
+      await readLine.question(
+        "Empty strings are invalid\nPlease enter a valid item name:\n" +
+          (oldItem ? `(${oldItem.name}) ` : ""),
+      )
+    ).trim();
+  }
+  let price = (
+    await readLine.question(
+      "Enter the item price:\n" + (oldItem ? `(${oldItem.price}) ` : ""),
+    )
+  ).trim();
+  let floatRegEx = /^[+]?[0-9]*\.?[0-9]+$/;
+  while (!floatRegEx.test(price)) {
+    price = (
+      await readLine.question(
+        "Invalid value\nPlease enter a valid item price:\n" +
+          (oldItem ? `(${oldItem.price}) ` : ""),
+      )
+    ).trim();
+  }
+  price = parseFloat(parseFloat(price).toFixed(2));
+  let store = (
+    await readLine.question(
+      "Enter the item store:\n" + (oldItem ? `(${oldItem.store}) ` : ""),
+    )
+  ).trim();
+  while (store === "") {
+    store = (
+      await readLine.question(
+        "Empty strings are invalid\nPlease enter a valid store name:\n" +
+          (oldItem ? `(${oldItem.store}) ` : ""),
+      )
+    ).trim();
+  }
+
+  return {
+    name,
+    price,
+    store,
+  };
 }
 
 /**
  * Starts a loop to read commands from the console inputs
  */
-function startCommandLoop(){
-    const rl = readline.createInterface({
-        input:process.stdin,
-        output:process.stdout,
-    });
-    function readCommand(){
-        rl.question("Enter a command:\nCreate\tRead\tUpdate\tDelete\tSummary\tExport\tExit\n").then(async(answer)=>{
-            switch(answer.toLowerCase()){
-                case "create":
-                    let createItem = {
-                        name:"",
-                        price:0,
-                        store:"",
-                    };
-                    rl.question("Enter the item name:\n").then(answer=>{
-                        createItem.name=answer;
-                        return rl.question("insert the item price:\n");
-                    }).then(answer=>{
-                        createItem.price=parseFloat(answer);
-                        if(!createItem.price){
-                            throw new Error("Invalid numeric value, operation cancelled\n");
-                        }else
-                            return rl.question("Inser the item store:\n");
-                    }).then(async answer=>{
-                        createItem.store=answer;
-                        await createWishlistItem(createItem);
-                        readCommand();
-                    }).catch(err=>{
-                        rl.write(err.message);
-                        readCommand();
-                    });
-                    break;
-                case "read":
-                    await readWishlist();
-                    readCommand();
-                    break;
-                case "update":
-                    let updateId=-1;
-                    let updateItem={
-                        name:"",
-                        price:0,
-                        store:"",
-                    };
-                    rl.question("Enter the item id:\n").then( async answer=>{
-                        updateId=parseInt(answer);
-                        if(!updateId){
-                            throw new Error("Invalid number value, operation cancelled\n");
-                        }
-                        return rl.question("Enter the item name:\n");
-                    }).then(answer=>{
-                        updateItem.name=answer;
-                        return rl.question("Enter the item price:\n");
-                    }).then(answer=>{
-                        updateItem.price=parseFloat(answer);
-                        if(!updateItem.price){
-                            throw new Error("Invalid number value, operation cancelled\n");
-                        }
-                        return rl.question("Insert the item store:\n");
-                    }).then(async answer=>{
-                        updateItem.store=answer;
-                        await updateWishlistItem(updateId,updateItem);
-                        readCommand();
-                    }).catch(err=>{
-                        rl.write(err.message);
-                        readCommand();
-                    });
-                    break;
-                case "delete":
-                    rl.question("Enter the item id:\n").then(async answer=>{
-                        if(!parseInt(answer)){
-                            rl.write("Invalid number value, operation cancelled\n");
-                            readCommand();
-                        }else{
-                            await removeWishlistItem(parseInt(answer));
-                            readCommand();
-                        }
-                    });
-                    break;
-                case "summary":
-                    await showWishlistSummary();
-                    readCommand();
-                    break;
-                case "export":
-                    await exportAsCSV();
-                    readCommand();
-                    break;
-                case "exit":
-                    rl.write("Have a nice day!");
-                    process.exit(0);
-                default:
-                    rl.write("Please enter a valid command\n");
-                    readCommand();
-                    break;
-            }
-        });
+function startCommandLoop() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  async function readCommand() {
+    let answer = (
+      await rl.question(
+        "Enter a command:\nCreate\tRead\tUpdate\tDelete\tSummary\tExport\tExit\n",
+      )
+    ).trim();
+    let consoleMessage = "";
+    switch (answer.toLowerCase()) {
+      case "create":
+        let createItem = await createItemOnConsole(rl);
+        consoleMessage = await createWishlistItem(createItem);
+        break;
+      case "read":
+        consoleMessage = await readWishlist();
+        break;
+      case "update":
+        let updateItemId = (await rl.question("Enter the item id:\n")).trim();
+        if (/^\d+$/.test(updateItemId)) {
+          let oldItem = await getItemById(parseInt(updateItemId));
+          if (oldItem != null) {
+            console.log(
+              `Name: ${oldItem.name}\tPrice: ${oldItem.price.toFixed(2)}\tStore: ${oldItem.store}`,
+            );
+            let updateItem = await createItemOnConsole(rl, oldItem);
+            consoleMessage = await updateWishlistItem(
+              parseInt(updateItemId),
+              updateItem,
+            );
+          } else {
+            consoleMessage = "Item not found";
+          }
+        } else consoleMessage = "Invalid numeric value for id";
+        break;
+      case "delete":
+        let deleteItemId = (await rl.question("Enter the item id:\n")).trim();
+        if (/^\d+$/.test(deleteItemId)) {
+          consoleMessage = await removeWishlistItem(parseInt(deleteItemId));
+        } else {
+          consoleMessage = "Invalid value for id, operation cancelled\n";
+        }
+        break;
+      case "summary":
+        consoleMessage = await showWishlistSummary();
+        break;
+      case "export":
+        consoleMessage = await exportAsCSV();
+        break;
+      case "exit":
+        rl.write("Have a nice day!");
+        process.exit(0);
+      default:
+        consoleMessage = "Please enter a valid command\n";
+        break;
     }
-    readCommand();
+    console.log(consoleMessage);
+    setTimeout(() => {
+      readCommand();
+    }, 1500);
+  }
+  readCommand();
 }
 
 //Make sure the wishlist exists, otherwise create it
-fs.open('wishlist.json',"a+").then(file=>{
-    file.read().then(content=>{
-        if(!content.buffer.find(val=>val!=0)){
-            file.write(JSON.stringify({
-                autoincrement:1,
-                items:[],
-            }));
-        }
-        file.close();
-    })
+await fs.open("wishlist.json", "a+").then((file) => {
+  file.read().then((content) => {
+    if (!content.buffer.find((val) => val != 0)) {
+      file.write(
+        JSON.stringify({
+          autoincrement: 1,
+          items: [],
+        }),
+      );
+    }
+    file.close();
+  });
 });
 
-if(process.argv.length<3){//Show the welcome message when no aditional arg is passed
-    console.log(welcomeMessage);
-    startCommandLoop();
-}else{
-    commandLineInterpreter();
+if (process.argv.length < 3) {
+  console.log(welcomeMessage);
+  startCommandLoop();
+} else {
+  commandLineInterpreter();
 }
