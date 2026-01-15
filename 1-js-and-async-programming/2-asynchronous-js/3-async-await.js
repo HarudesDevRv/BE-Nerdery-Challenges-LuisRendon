5290d68e47a7ec40d6b61fb27c228fdfb5202fca
HarudesDevRv <luisrendon@ravn.co>
2026-01-15 09:05:50 -0500

    Refactor: Changed the code to make a right use of await usage in asynchronous code

--- 1-js-and-async-programming/2-asynchronous-js/3-async-await.js
+++ 1-js-and-async-programming/2-asynchronous-js/3-async-await.js
@@ -11,7 +11,12 @@
     - Make sure to return a string containing the name of the most common subscription
 */
 
-const { getLikedMovies, getUsers, getDislikedMovies, getUserSubscriptionByUserId } = require("./utils/mocked-api");
+const {
+  getLikedMovies,
+  getUsers,
+  getDislikedMovies,
+  getUserSubscriptionByUserId,
+} = require("./utils/mocked-api");
 /**
  * Logs the most common subscription among users
  * who disliked more movies than they liked.
@@ -19,46 +24,46 @@ const { getLikedMovies, getUsers, getDislikedMovies, getUserSubscriptionByUserId
  * @returns {Promise<string>} Logs the subscription name as a string.
  */
 const getCommonDislikedSubscription = async () => {
-  //Retrieve the users with more dislikes than likes similarly to the previous excercise
-  let users = await getUsers();
+  //Retrieve the users with more dislikes than likes similarly to the previous exercise
+  let [users, userLikedMovies, userDislikedMovies] = await Promise.all([
+    getUsers(),
+    getLikedMovies(),
+    getDislikedMovies(),
+  ]);
 
-  let userLikedMovies = await getLikedMovies().then(users=>users.map(user=>user.movies.length));
-  
-  let userDislikedMovies = await getDislikedMovies().then(users=>users.map(user=>user.movies.length));
-  
-  let usersWithMoreDislikedMovies=[];
- 
-  for(let i=0; i<users.length; i++){
-    if(userDislikedMovies[i]>userLikedMovies[i])
+  let usersWithMoreDislikedMovies = [];
+
+  for (let i = 0; i < users.length; i++) {
+    if (userDislikedMovies[i].movies.length > userLikedMovies[i].movies.length)
       usersWithMoreDislikedMovies.push(users[i]);
   }
-  //Set a map to store how many "haters" 
-  let hatersBySubscription = new Map();
 
-  for(let user of usersWithMoreDislikedMovies){
-    //Await for each user subscription data
-    //to keep the track for each kind of subscription without problems
-    await getUserSubscriptionByUserId(user.id).then(
-      subscription=>{
-        let subscriptionType = subscription.subscription;
-        if(hatersBySubscription.has(subscriptionType)){
-          hatersBySubscription.set(subscriptionType,hatersBySubscription.get(subscriptionType)+1);
-        }else{
-          hatersBySubscription.set(subscriptionType,1);
-        }
-      }
+  let hatersBySubscription = [];
+  for (let user of usersWithMoreDislikedMovies) {
+    //Await for each user subscription data and count the haters by subscription
+    let userSubscription = await getUserSubscriptionByUserId(user.id);
+    userSubscription = userSubscription.subscription;
+    let subscriptionIndex = hatersBySubscription.findIndex(
+      (subscription) => subscription.name == userSubscription,
     );
+    if (subscriptionIndex >= 0) {
+      hatersBySubscription[subscriptionIndex].count += 1;
+    } else {
+      hatersBySubscription.push({
+        name: userSubscription,
+        count: 1,
+      });
+    }
   }
   //Get the highest "haters" amount and return its subscription
-  let maxHaters = Math.max(...hatersBySubscription.values()); 
-
-  for(let subscription of hatersBySubscription){
-    if(subscription[1]===maxHaters){
-      return subscription[0];
+  let result = { name: "none", count: 0 };
+  for (let subscription of hatersBySubscription) {
+    if (subscription.count > result.count) {
+      result = subscription;
     }
   }
 
-  return "None";
+  return result.name;
 };
 
 getCommonDislikedSubscription().then((subscription) => {
