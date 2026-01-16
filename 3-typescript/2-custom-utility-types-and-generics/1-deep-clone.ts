247a064806bea8b2bcb6613d179e30e6a4c0ab89
HarudesDevRv <luisrendon@ravn.co>
2026-01-16 15:33:45 -0500

    Feat: Finished the initial implementation of the Deep clone function

--- 3-typescript/2-custom-utility-types-and-generics/1-deep-clone.ts
+++ 3-typescript/2-custom-utility-types-and-generics/1-deep-clone.ts
@@ -14,31 +14,51 @@
 //? implement the function  here
 type Primitive = number | string | boolean | undefined | null;
 
-function deepCopy<T>(variable: T): T {
-  switch (typeof variable) {
+type ElementType<T> = T extends (infer U)[] ? U : any;
+
+function deepClone<T extends Primitive | object>(clonedObject: T): T {
+  switch (typeof clonedObject) {
     case "undefined":
     case "boolean":
     case "number":
     case "string":
-      return variable;
+      //If its a primitive just return it
+      return clonedObject;
     case "object":
-      if (Array.isArray(variable)) {
-        let returnArray = [];
-        for (let value of variable) {
-          returnArray.push(deepCopy(value));
+      if (Array.isArray(clonedObject)) {
+        //If its an array, create a new empty array of inferred type
+        let returnArray: ElementType<T>[] = [];
+        for (let value of clonedObject) {
+          //Recursively fill the array to handle nested objects
+          returnArray.push(deepClone(value));
         }
+        return returnArray as T;
       } else {
-        let returnObject = {};
-
-        for (let property of Object.entries(variable!)) {
+        if (clonedObject) {
+          //If its an object, duplicate the original one first
+          let returnObject: T = { ...clonedObject };
+          let keys = Object.keys(clonedObject);
+          (Object.keys(clonedObject) as (keyof T)[]).forEach(
+            (propertyKey, i) => {
+              //Recursively assign the properties to handle nested objects
+              let propertyValue = (clonedObject as { [key: string]: any })[
+                keys[i]
+              ];
+              Object.assign(returnObject, {
+                [propertyKey]: deepClone(propertyValue),
+              });
+            },
+          );
+          return returnObject;
         }
       }
   }
-  return variable;
+  return clonedObject; //If it didn't satisfy the switch case it must be null
 }
 
-function deepClone<T>(object: T): T {
-  return deepCopy(object);
+interface Item {
+  name: string;
+  ids: number[];
 }
 
 interface Test {
@@ -46,8 +66,10 @@ interface Test {
   id: number;
   price: number;
   keys: string[];
-  items: object[];
+  items: Item[];
+  mostExpensiveItem: Item;
   undef: undefined;
+  nullable: string | null;
 }
 
 let testVariable: Test = {
@@ -55,8 +77,49 @@ let testVariable: Test = {
   id: 1232,
   price: 123.231,
   keys: ["first", "second", "third", "fourth"],
-  items: [{}, {}],
+  items: [
+    { name: "test item 1", ids: [1, 2, 3] },
+    { name: "test item 2", ids: [4, 5, 6] },
+  ],
+  mostExpensiveItem: { name: "test item 4", ids: [10, 20, 30] },
+  nullable: null,
   undef: undefined,
 };
 
 let secondVariable = deepClone(testVariable);
+
+secondVariable.id = 1211;
+
+secondVariable.name = "Second";
+
+secondVariable.price = 542.23;
+
+secondVariable.keys.push("fifth");
+
+secondVariable.keys[3] = "No longer fourth";
+
+secondVariable.items.push({
+  name: "test item 3",
+  ids: [7, 8, 9],
+});
+
+secondVariable.items[0].name = "no longer item 1";
+secondVariable.items[0].ids[0] = 0;
+secondVariable.items[0].ids.push(13);
+
+secondVariable.items[1].ids = [25, 36, 49];
+
+secondVariable.mostExpensiveItem.name = "no longer item 4";
+secondVariable.mostExpensiveItem.ids = [10, 11, 12];
+
+console.log("First object");
+console.log(testVariable);
+
+console.log("Second object");
+console.log(secondVariable);
+
+console.log("First item");
+console.log(testVariable.items[0].ids);
+console.log(secondVariable.items[0].ids);
+console.log(testVariable.items[1].ids);
+console.log(secondVariable.items[1].ids);
